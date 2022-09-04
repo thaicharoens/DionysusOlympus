0472c56c6e65f856cea87bcc3641f54924158dfc
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2022-09-04 11:10:06 +0000

    Now generates pubsub token with group assignments

--- api/controllers/pubsub.js
+++ api/controllers/pubsub.js
@@ -1,5 +1,5 @@
 /**
- * tickets/claim
+ * pubsub
  *
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
@@ -10,8 +10,14 @@ module.exports = {
   fn: async function () {
     const { req, res } = this;
     const decodedJWT = await sails.helpers.aadB2C.with({ jwt: req.headers.authorization.split('Bearer ')[1] });
-    //    const agentAuthorisedEvents = await Agent.find({ user: decodedJWT.sub, role:'SCAN_AGENT' });
-    let pubsubToken = await sails.helpers.pubsubClient().getClientAccessToken( { userId: decodedJWT.sub });
+    const pubsubGroups = [];
+    const agentAuthorisedEvents = await Agent.find({ user: decodedJWT.sub, role:'SCAN_AGENT' });
+    [...new Set(agentAuthorisedEvents.map((agent) => {
+      return agent.event;
+    }))].forEach((event) => {
+      pubsubGroups.push(`event_${event}_scanned`);
+    });
+    let pubsubToken = await sails.helpers.pubsubClient().getClientAccessToken( { userId: decodedJWT.sub,groups: pubsubGroups } );
 
     return res.status(200).json({
       status: true,
